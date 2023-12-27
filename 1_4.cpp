51a3d004593e2c5d9d89425a784e2fb3735f3f69
Roman Abaghayn <84144854+777ARM777@users.noreply.github.com>
2023-12-27 23:06:06 +0400

    Delete 1_4.cpp

--- 1_4.cpp
+++
@@ -1,7 +0,0 @@
-friend set union_sets(set& s1, set& s2) {
-    set result = s1;
-    for (treeNode<T>* i = s2.t->begin(); i != s2.t->end(); i = s2.t->inorderNext()) {
-        result.t->insert(i->data);
-    }
-    return result;
-}
